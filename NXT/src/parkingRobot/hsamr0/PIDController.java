8ee122a3587786e4846ece2f656397656f6690aa
jonas <gurokrei@gmail.com>
2020-12-02 14:02:34 +0100

    Implemented PID-Algorithm

--- NXT/src/parkingRobot/hsamr0/PIDController.java
+++ NXT/src/parkingRobot/hsamr0/PIDController.java
@@ -1,23 +1,57 @@
 package parkingRobot.hsamr0;
 
+import lejos.nxt.LCD;
+
 public class PIDController {
-	
+
 	/**
 	 * Calculates the amount of change required to bring the process variable to its
 	 * setpoint.
 	 * 
-	 * @param data 
-	 * @return The amount of power change required to bring the process variable to its
-	 * 	setpoint. 
+	 * @param data
+	 * @return The amount of power change required to bring the process variable to
+	 *         its setpoint.
 	 */
-	
+
 	static double pi_ctrl(PIDData data) {
 		double error = data.setpoint - data.processVariable;
+
+		data.integral = data.integral + error;
+
+		double result = error * data.param_p + data.integral * data.param_i;
+
+		return result;
+	}
+
+	static double pd_ctrl(PIDData data) {
+		double error = data.setpoint - data.processVariable;
+		double derivate = (error - data.error_prev) / (System.currentTimeMillis() - data.millis_prev);
+
+		double result = error * data.param_p + derivate * data.param_d;
 		
+		data.millis_prev = System.currentTimeMillis();
+
+		return result;
+	}
+	
+	static double pid_ctrl(PIDData data) {
+		double error = data.setpoint - data.processVariable;
+		double derivate = (error - data.error_prev) / (System.currentTimeMillis() - data.millis_prev);
+
 		data.integral = data.integral + error;
 		
-		double result = error * data.param_p + data.integral * data.param_i;
+		if (error > data.error_max) {
+			data.error_max = error;
+		}
 		
+		data.error_prev = error;
+		
+		//LCD.drawString("e_max = " + data.error_max, 0, 5);
+		
+		double result = error * data.param_p + derivate * data.param_d + data.integral * data.param_i;
+		
+		data.millis_prev = System.currentTimeMillis();
+
 		return result;
 	}
 }
--- NXT/src/parkingRobot/hsamr0/PIDData.java
+++ NXT/src/parkingRobot/hsamr0/PIDData.java
@@ -5,13 +5,50 @@ public class PIDData {
 	public double processVariable = 0;
 	public double param_p = 0;
 	public double param_i = 0;
+	public double param_d = 0;
 	
 	public double integral = 0;
 	
-	public PIDData(double sp, double pv, double kp, double ki) {
-		setpoint = sp;
-		processVariable = pv;
-		param_p = kp;
-		param_i = ki;
+	public double error_prev = 0;
+	public long millis_prev = 0;
+	
+	public double error_max = 0;
+
+	public PIDData() {
+		millis_prev = System.currentTimeMillis();
+	}
+	
+	public static PIDData pi(double sp, double pv, double kp, double ki) {
+		PIDData result = new PIDData();
+		
+		result.setpoint = sp;
+		result.processVariable = pv;
+		result.param_p = kp;
+		result.param_i = ki;
+		
+		return result;
+	}
+	
+	public static PIDData pd(double sp, double pv, double kp, double kd) {
+		PIDData result = new PIDData();
+		
+		result.setpoint = sp;
+		result.processVariable = pv;
+		result.param_p = kp;
+		result.param_d = kd;
+		
+		return result;
+	}
+	
+	public static PIDData pid(double sp, double pv, double kp, double ki, double kd) {
+		PIDData result = new PIDData();
+		
+		result.setpoint = sp;
+		result.processVariable = pv;
+		result.param_p = kp;
+		result.param_i = ki;
+		result.param_d = kd;
+		
+		return result;
 	}
 }
