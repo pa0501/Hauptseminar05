8ffd93a3957ded4eb3b2fe0b021fcbcb3c49be63
jonas <jonas@DESKTOP-6DH6DIV>
2020-12-17 18:27:37 +0100

    Changed so velocity can be tested.

--- NXT/src/parkingRobot/hsamr0/Test_PathFollow.java
+++ NXT/src/parkingRobot/hsamr0/Test_PathFollow.java
@@ -21,27 +21,33 @@ import parkingRobot.hsamr0.PerceptionPMP;
 
 //PUSH TEST v2
 /**
- * Main class for 'Hauptseminar AMR' project 'autonomous parking' for students of electrical engineering
- * with specialization 'automation, measurement and control'.
+ * Main class for 'Hauptseminar AMR' project 'autonomous parking' for students
+ * of electrical engineering with specialization 'automation, measurement and
+ * control'.
  * <p>
- * Task of the robotic project is to develop an mobile robot based on the Lego NXT system witch can perform
- * parking maneuvers on an predefined course. To fulfill the interdisciplinary aspect of this project the software
- * structure is divided in 5 parts: human machine interface, guidance, control, perception and navigation.
+ * Task of the robotic project is to develop an mobile robot based on the Lego
+ * NXT system witch can perform parking maneuvers on an predefined course. To
+ * fulfill the interdisciplinary aspect of this project the software structure
+ * is divided in 5 parts: human machine interface, guidance, control, perception
+ * and navigation.
  * <p>
- * Guidance is to be realized in this main class. The course of actions is to be controlled by one or more finite
- * state machines (FSM). It may be advantageous to nest more than one FSM.
+ * Guidance is to be realized in this main class. The course of actions is to be
+ * controlled by one or more finite state machines (FSM). It may be advantageous
+ * to nest more than one FSM.
  * <p>
- * For the other parts there are interfaces defined and every part has to be realized in one main module class.
- * Every class (except guidance) has additionally to start its own thread for background computation.
+ * For the other parts there are interfaces defined and every part has to be
+ * realized in one main module class. Every class (except guidance) has
+ * additionally to start its own thread for background computation.
  * <p>
- * It is important that data witch is accessed by more than one main module class thread is only handled in a
- * synchronized context to avoid inconsistent or corrupt data!
+ * It is important that data witch is accessed by more than one main module
+ * class thread is only handled in a synchronized context to avoid inconsistent
+ * or corrupt data!
  */
 public class Test_PathFollow {
-	
+
 	/**
-	 * states for the main finite state machine. This main states are requirements because they invoke different
-	 * display modes in the human machine interface.
+	 * states for the main finite state machine. This main states are requirements
+	 * because they invoke different display modes in the human machine interface.
 	 */
 	public enum CurrentStatus {
 		/**
@@ -57,101 +63,107 @@ public class Test_PathFollow {
 		 */
 		EXIT
 	}
-	
-	
+
 	/**
 	 * state in which the main finite state machine is running at the moment
 	 */
-	protected static CurrentStatus currentStatus 	= CurrentStatus.INACTIVE;
+	protected static CurrentStatus currentStatus = CurrentStatus.INACTIVE;
 	/**
-	 * state in which the main finite state machine was running before entering the actual state
+	 * state in which the main finite state machine was running before entering the
+	 * actual state
 	 */
-	protected static CurrentStatus lastStatus		= CurrentStatus.INACTIVE;
-	
-	
+	protected static CurrentStatus lastStatus = CurrentStatus.INACTIVE;
+
 	/**
-	 * one line of the map of the robot course. The course consists of a closed chain of straight lines.
-	 * Thus every next line starts where the last line ends and the last line ends where the first line starts.
-	 * This documentation for line0 hold for all lines.
+	 * one line of the map of the robot course. The course consists of a closed
+	 * chain of straight lines. Thus every next line starts where the last line ends
+	 * and the last line ends where the first line starts. This documentation for
+	 * line0 hold for all lines.
 	 */
-	static Line line0 = new Line(  0,  0, 180,  0);
-	static Line line1 = new Line(180,  0, 180, 60);
+	static Line line0 = new Line(0, 0, 180, 0);
+	static Line line1 = new Line(180, 0, 180, 60);
 	static Line line2 = new Line(180, 60, 150, 60);
 	static Line line3 = new Line(150, 60, 150, 30);
-	static Line line4 = new Line(150, 30,  30, 30);
-	static Line line5 = new Line( 30, 30,  30, 60);
-	static Line line6 = new Line( 30, 60,   0, 60);
-	static Line line7 = new Line(  0, 60,   0,  0);
+	static Line line4 = new Line(150, 30, 30, 30);
+	static Line line5 = new Line(30, 30, 30, 60);
+	static Line line6 = new Line(30, 60, 0, 60);
+	static Line line7 = new Line(0, 60, 0, 0);
 	/**
-	 * map of the robot course. The course consists of a closed chain of straight lines.
-	 * Thus every next line starts where the last line ends and the last line ends where the first line starts.
-	 * All above defined lines are bundled in this array and to form the course map.
+	 * map of the robot course. The course consists of a closed chain of straight
+	 * lines. Thus every next line starts where the last line ends and the last line
+	 * ends where the first line starts. All above defined lines are bundled in this
+	 * array and to form the course map.
 	 */
-	static Line[] map = {line0, line1, line2, line3, line4, line5, line6, line7};
-	
-	
+	static Line[] map = { line0, line1, line2, line3, line4, line5, line6, line7 };
+
 	/**
 	 * main method of project 'ParkingRobot'
 	 * 
 	 * @param args standard string arguments for main method
 	 * @throws Exception exception for thread management
 	 */
-	public static void main(String[] args) throws Exception {		
-        currentStatus = CurrentStatus.INACTIVE;
-        lastStatus    = CurrentStatus.EXIT;
-		
+	public static void main(String[] args) throws Exception {
+		currentStatus = CurrentStatus.INACTIVE;
+		lastStatus = CurrentStatus.EXIT;
+
 		// Generate objects
-		
-		NXTMotor leftMotor  = new NXTMotor(MotorPort.B);
+
+		NXTMotor leftMotor = new NXTMotor(MotorPort.B);
 		NXTMotor rightMotor = new NXTMotor(MotorPort.A);
-		
+
 		IMonitor monitor = new Monitor();
-		
+
 		IPerception perception = new PerceptionPMP(leftMotor, rightMotor, monitor);
 		perception.calibrateLineSensors();
-		
+
 		INavigation navigation = new NavigationAT(perception, monitor);
-		IControl    control    = new ControlRST(perception, navigation, leftMotor, rightMotor, monitor);
+		IControl control = new ControlRST(perception, navigation, leftMotor, rightMotor, monitor);
 
 		monitor.startLogging();
-		
+
 		control.setStartTime((int) System.currentTimeMillis());
-		control.setDestination(0, 0.1, 0.5);
+		control.setVelocity(0.05);
+
+		// Beispielhaft die Endposition, wenn der Roboter mit Sensor 1 das Hindernis
+		// sieht und nach vorne einparkt
+		control.setDestination(0, 0.25, 0.2);
 		control.setCtrlMode(IControl.ControlMode.PARK_CTRL);
-		
+
 		LCD.clear();
-		
-		while(true) {
+
+		while (true) {
 			if (Button.ESCAPE.isDown()) {
-				System.exit(0);;
-			}     	
+				System.exit(0);
+				;
+			}
 		}
 	}
-	
-	
+
 	/**
-	 * returns the actual state of the main finite state machine as defined by the requirements
+	 * returns the actual state of the main finite state machine as defined by the
+	 * requirements
 	 * 
 	 * @return actual state of the main finite state machine
 	 */
-	public static CurrentStatus getCurrentStatus(){
+	public static CurrentStatus getCurrentStatus() {
 		return Test_PathFollow.currentStatus;
 	}
-	
+
 	/**
 	 * plots the actual pose on the robots display
 	 * 
-	 * @param navigation reference to the navigation class for getting pose information
+	 * @param navigation reference to the navigation class for getting pose
+	 *                   information
 	 */
-	protected static void showData(INavigation navigation, IPerception perception){
-		LCD.clear();	
-		
-		LCD.drawString("X (in cm): " + (navigation.getPose().getX()*100), 0, 0);
-		LCD.drawString("Y (in cm): " + (navigation.getPose().getY()*100), 0, 1);
-		LCD.drawString("Phi (grd): " + (navigation.getPose().getHeading()/Math.PI*180), 0, 2);
-		
+	protected static void showData(INavigation navigation, IPerception perception) {
+		LCD.clear();
+
+		LCD.drawString("X (in cm): " + (navigation.getPose().getX() * 100), 0, 0);
+		LCD.drawString("Y (in cm): " + (navigation.getPose().getY() * 100), 0, 1);
+		LCD.drawString("Phi (grd): " + (navigation.getPose().getHeading() / Math.PI * 180), 0, 2);
+
 //		perception.showSensorData();
-		
+
 //    	if ( hmi.getMode() == parkingRobot.INxtHmi.Mode.SCOUT ){
 //			LCD.drawString("HMI Mode SCOUT", 0, 3);
 //		}else if ( hmi.getMode() == parkingRobot.INxtHmi.Mode.PAUSE ){
