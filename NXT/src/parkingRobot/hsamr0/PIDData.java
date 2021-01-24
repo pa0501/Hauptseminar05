3a05847d57281316418896017805b8b6fb2c5515
lonelypiscis <gurokrei@gmail.com>
2021-01-24 20:02:16 +0100

    Added fields for storing max values

--- NXT/src/parkingRobot/hsamr0/PIDData.java
+++ NXT/src/parkingRobot/hsamr0/PIDData.java
@@ -7,6 +7,10 @@ public class PIDData {
 	public double param_i = 0;
 	public double param_d = 0;
 	
+	public double output_max = 0;
+	public double i_max = 0;
+	public boolean cap_output, cap_i;
+	
 	public double integral = 0;
 	
 	public double error_prev = 0;
@@ -16,6 +20,26 @@ public class PIDData {
 
 	public PIDData() {
 		millis_prev = System.currentTimeMillis();
+	}
+	
+	public PIDData cap_output(double max) {
+		cap_output = true;
+		output_max = max;
+		
+		return this;
+	}
+	
+	public PIDData cap_i() {
+		cap_i = true;
+		
+		return this;
+	}
+	
+	public PIDData cap_i(double max) {
+		i_max = max;
+		cap_i = true;
+		
+		return this;
 	}
 	
 	public static PIDData pi(double sp, double pv, double kp, double ki) {
